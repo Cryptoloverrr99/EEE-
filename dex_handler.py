164228f236efb4f84c429e8043ab8d4badee6af9
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-04 20:06:49 +0000

    Update dex_handler.py

--- dex_handler.py
+++ dex_handler.py
@@ -1,6 +1,6 @@
+from tenacity import retry, stop_after_attempt, wait_fixed
 import requests
 from config import *
-from tenacity import retry, stop_after_attempt, wait_fixed
 
 def get_dex_data():
     @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
