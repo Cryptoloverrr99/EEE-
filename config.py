24ad6eca2909312469e5aeade2d75ff6f7d842a6
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-04 09:56:17 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+DEXSCREENER_API = "https://api.dexscreener.com/token-boosts/latest/v1"
+SOLSCAN_API = "https://pro-api.solscan.io/v2.0/token/meta"
+RUGCHECK_API = "https://api.rugcheck.xyz/v1/stats/new_tokens"
+
+MIN_MARKET_CAP = 150000
+MIN_LIQUIDITY = 70000
+MIN_VOLUME = 100000
+
+TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+TELEGRAM_CHANNEL = "@YOUR_CHANNEL_NAME"
